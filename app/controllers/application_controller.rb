3226512ffb6c5e485840cb662495f8f1e97329f2
Harangus <vencacvrcov@gmail.com>
2025-06-07 12:55:12 +0200

    Editing user details using devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :avatar])
   end
 end
