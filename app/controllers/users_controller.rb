42ee51c86674c5aef8e1f51539945a0c24faf585
Harangus <vencacvrcov@gmail.com>
2025-06-16 15:49:46 +0200

    better design, issues for user in user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
 
   def show
     @tasks = @user.tasks.includes(:user).order(created_at: :desc)
+    @issues = @user.issues.includes(:user).order(created_at: :desc)
   end
 
   def edit
